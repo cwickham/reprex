719a91efe55aa795855d6ddf892112593e3f2dff
Lionel Henry <lionel.hry@gmail.com>
2019-01-05 15:44:27 +0100

    Entrace base errors

--- inst/templates/REPREX.R
+++ inst/templates/REPREX.R
@@ -4,7 +4,12 @@
 
 #+ reprex-setup, include = FALSE
 options(tidyverse.quiet = {{{tidyverse_quiet}}})
-knitr::opts_chunk$set(collapse = TRUE, comment = "{{{comment}}}", error = TRUE)
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "{{{comment}}}",
+  error = TRUE,
+  calling.handlers = list(error = rlang::entrace)
+)
 knitr::opts_knit$set(upload.fun = {{{upload_fun}}})
 
 #+ reprex-body
